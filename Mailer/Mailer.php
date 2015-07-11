36d9c951763e68010bc525be7088b2f519e6a0ce
git <brother79@mail.ru>
2015-07-11 18:21:12 +0600

    Mailer

--- Mailer/Mailer.php
+++ Mailer/Mailer.php
@@ -20,39 +20,5 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
  */
 class Mailer extends BaseMailer
 {
-    protected $mailer;
 
-    /**
-     * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface   $dispatcher
-     * @param \Swift_Mailer                                                 $mailer
-     * @param EngineInterface                                               $templating
-     * @param array                                                         $config
-     */
-    public function __construct(EventDispatcherInterface $dispatcher, \Swift_Mailer $mailer, EngineInterface $templating, $config)
-    {
-        parent::__construct($dispatcher, $templating, $config);
-
-        $this->mailer = $mailer;
-    }
-
-
-    /**
-     * @param array $options
-     */
-    public function sendEmail(array $options)
-    {
-        if (null !== $this->mailer) {
-            $message = \Swift_Message::newInstance()
-                ->setSubject($options['subject'])
-                ->setFrom($options['from'])
-                ->setTo($options['to'])
-                ->setBody($options['body']);
-
-            if (isset($options['cc'])) {
-                $message->setCc($options['body']);
-            }
-
-            $this->mailer->send($message);
-        }
-    }
 }
