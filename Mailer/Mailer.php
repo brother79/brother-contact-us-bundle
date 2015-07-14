e31d1ac908a5a080cfb3f05fedae3fb1e228ea48
git <brother79@mail.ru>
2015-07-14 15:25:57 +0600

    guestbook

--- Mailer/Mailer.php
+++ Mailer/Mailer.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the BrotherContactUsBundle package
- *
- * (c) Yos Okus <yos.okusanya@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Brother\ContactUsBundle\Mailer;
 
 use Brother\CommonBundle\Mailer\BaseMailer;
